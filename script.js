5fa14f6028f0278e310a274e8d09cda56adbe577
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-10-24 21:46:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
-
-function matches(n) {
-    let str = " ";
-    // for (let i = 0; i < n; i++) {
-    //     str += "   |";
-    // }
-    alert(n + str);
-    function choose(m) {
-        while (n > 0) {
-            alert("You turn!");
-            m = prompt("Get the matches")
-            n -= m;
-            for (let i = 0; i < n; i++) {
-                str += "   |";
-            }
-            if (isNaN(m) || m < 0 || m > 3) {
-                break;
-            }
-            if (n % 4 == 0) {
-                alert("You win!");
-                break;
-            }
-            else {
-                alert(n + str);
-            }
-            if (n <= 0) {
-                alert("You win!");
-                break;
-            }
-            alert("My turn");
-            let rand = Math.floor(Math.random() * 3) + 1;
-            n -= rand;
-            if (n % 4 == 0) {
-                alert("I win!");
-                break;
-            }
-            else {
-                alert(n + str);
-            }
-        }
-    }
-    choose()
+let str = " ";
+function start(n) {
+    // alert(n);
+    matches(n)
 }
-matches(n = prompt("Введите количество спичек"));
+start(n = prompt("Введите количество спичек"));
+function matches() {
+    for (let i = 0; i < n; i++) {
+        str += " | ";
+    }
+    alert(n + str);
+}
+while (n > 0) {
+    alert("Your turn!");
+    let m = prompt("Get the matches!");
+    if (isNaN(m) || m < 0 || m > 3) {
+        break;
+    }
+    n -= m;
+    // let str = " ";
+    // for (let i = 0; i < n; i++) {
+    //     str += "| ";
+    // }
+    // alert(n + str);
+    matches(n)
+    if (n <= 0) {
+        alert("you win!");
+        break;
+    }
+    alert("my turn!");
+    let rand = Math.floor(Math.random() * 3) + 1;
+    n -= rand;
+    matches(n)
+    // alert(n + str);
+}
